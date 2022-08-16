c9ef765a5568067849bfdaa39b5c817e39c9dbf5
lautaromugas8 <lautaro_mugas@hotmail.com>
2022-08-16 18:23:13 -0300

    Added Modal component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from "next/head";
+import { useRecoilValue } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Modal from "../components/Modal";
 import Row from "../components/Row";
+import useAuth from "../hooks/useAuth";
 import { Movie } from "../typings";
 import requests from "../utils/requests";
 
@@ -26,10 +30,22 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
+  const { loading } = useAuth();
+  const showModal = useRecoilValue(modalState);
+  const movie = useRecoilValue(movieState);
+
+  if (loading) return null;
+
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && "!h-screen overflow-hidden"
+      }`}
+    >
       <Head>
-        <title>Home - Netflix</title>
+        <title>
+          {movie?.title || movie?.original_name || "Home"} - Netflix
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -45,8 +61,8 @@ const Home = ({
           <Row title="Romance" movies={romanceMovies} />
           <Row title="Documentaries" movies={documentaries} />
         </section>
-        {/* Modal */}
       </main>
+      {showModal && <Modal />}
     </div>
   );
 };
